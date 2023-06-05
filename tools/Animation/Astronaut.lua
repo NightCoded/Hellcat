26569278aeca067d86a806fe66829f485fd48f3f
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 12:06:36 +0700

    Update Astronaut.lua

--- tools/Animation/Astronaut.lua
+++ tools/Animation/Astronaut.lua
@@ -6,4 +6,4 @@ Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=891636393"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=891627522"
 Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=891609353"
 Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=891617961"
-game.Players.LocalPlayer.Character.Humanoid.Jump = true
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
