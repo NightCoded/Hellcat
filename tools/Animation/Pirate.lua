87359c579d570092fc8b4356b212742e444ab2b4
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 12:36:45 +0700

    Update Pirate.lua

--- tools/Animation/Pirate.lua
+++ tools/Animation/Pirate.lua
@@ -1,3 +1,5 @@
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+wait(2)
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=750781874"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=750782770"
@@ -6,4 +8,3 @@ Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=750783738"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=750782230"
 Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=750779899"
 Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=750780242"
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
