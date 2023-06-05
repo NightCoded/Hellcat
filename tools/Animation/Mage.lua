76d50e3a1729f4e0b45677f4a337e447ae35c1c2
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 12:35:41 +0700

    Update Mage.lua

--- tools/Animation/Mage.lua
+++ tools/Animation/Mage.lua
@@ -1,3 +1,5 @@
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+wait(2)
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=707742142"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=707855907"
@@ -6,4 +8,4 @@ Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=707861613"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=707853694"
 	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=707826056"
 	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=707829716"
-	game.Players.LocalPlayer.Character.Humanoid.Health = 0
+	
