8ed9f3c05fa97a280ae79ae6e04a9d2ce2d9ec02
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 09:16:00 +0700

    Create Superhero.lua

---
+++ tools/Animation/Superhero.lua
@@ -0,0 +1,9 @@
+local Animate = game.Players.LocalPlayer.Character.Animate
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=616111295"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=616113536"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616122287"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616117076"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=616115533"
+	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=616104706"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=616108001"
+	game.Players.LocalPlayer.Character.Humanoid.Jump = true
