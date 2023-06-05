82be18d265573d1fd500768183d4940f184dc08d
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 17:49:34 +0700

    Update Cartoony.lua

--- tools/Animation/Cartoony.lua
+++ tools/Animation/Cartoony.lua
@@ -1,6 +1,4 @@
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
-game.Players.LocalPlayer.CharacterAdded:Connect(function()
-wait(0.5)
+
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=742637544"
 	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=742638445"
@@ -9,4 +7,3 @@ Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=742637544
 	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=742637942"
 	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=742636889"
 	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=742637151"
-end)
