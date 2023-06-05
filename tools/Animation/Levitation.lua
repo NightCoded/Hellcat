f3e3a426b1f967e03311ed0c42d3c06a4706b6de
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 12:35:12 +0700

    Update Levitation.lua

--- tools/Animation/Levitation.lua
+++ tools/Animation/Levitation.lua
@@ -1,3 +1,5 @@
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+wait(2)
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=616006778"
 	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=616008087"
@@ -6,4 +8,4 @@ Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=616006778
 	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=616008936"
 	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=616003713"
 	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=616005863"
-	game.Players.LocalPlayer.Character.Humanoid.Health = 0
+	
