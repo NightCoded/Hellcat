12af6d8878be37e7f387d880227b113eff29eb14
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 09:18:11 +0700

    Create Cartoony.lua

---
+++ tools/Animation/Cartoony.lua
@@ -0,0 +1,9 @@
+local Animate = game.Players.LocalPlayer.Character.Animate
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=742637544"
+	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=742638445"
+	Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=742640026"
+	Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=742638842"
+	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=742637942"
+	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=742636889"
+	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=742637151"
+	game.Players.LocalPlayer.Character.Humanoid.Jump = true
