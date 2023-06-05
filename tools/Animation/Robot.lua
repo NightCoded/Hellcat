af574a844fec4d38e395cc2974202ee0c9c2eb09
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 09:11:22 +0700

    Create Robot.lua

---
+++ tools/Animation/Robot.lua
@@ -0,0 +1,9 @@
+local Animate = game.Players.LocalPlayer.Character.Animate
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=616088211"
+Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=616089559"
+Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=616095330"
+Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616091570"
+Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=616090535"
+Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=616086039"
+Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=616087089"
+game.Players.LocalPlayer.Character.Humanoid.Jump = true
