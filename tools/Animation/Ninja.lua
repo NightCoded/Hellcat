5765ed76ae87e293d9c3619dd8efa687307abc90
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 17:51:07 +0700

    Update Ninja.lua

--- tools/Animation/Ninja.lua
+++ tools/Animation/Ninja.lua
@@ -1,6 +1,4 @@
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
-game.Players.LocalPlayer.CharacterAdded:Connect(function()
-wait(0.5)
+
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=656117400"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=656118341"
@@ -9,4 +7,3 @@ Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=656118852"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=656117878"
 Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=656114359"
 Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=656115606"
-end)
