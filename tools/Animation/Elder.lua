8f5176832387cbdea172738f51e971053167e596
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 17:49:59 +0700

    Update Elder.lua

--- tools/Animation/Elder.lua
+++ tools/Animation/Elder.lua
@@ -1,6 +1,4 @@
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
-game.Players.LocalPlayer.CharacterAdded:Connect(function()
-wait(0.5)
+
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=845397899"
 	Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=845400520"
@@ -9,4 +7,3 @@ Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=845397899
 	Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=845398858"
 	Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=845392038"
 	Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=845396048"
-end)
