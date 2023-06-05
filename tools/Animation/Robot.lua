b6eae25c9abb51b950c9a3926ccd4202c12e4ba7
NightCode <133927804+NightCoded@users.noreply.github.com>
2023-06-05 12:37:42 +0700

    Update Robot.lua

--- tools/Animation/Robot.lua
+++ tools/Animation/Robot.lua
@@ -1,3 +1,5 @@
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
+wait(2)
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=616088211"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=616089559"
@@ -6,4 +8,4 @@ Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=616091570"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=616090535"
 Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=616086039"
 Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=616087089"
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
+
